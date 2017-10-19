67763d084b2d03881e97baf879f1a0d531dd8f24
Robert Brown <robert.brown@contentcoffee.com>
2017-10-19 12:06:24 +0200

    cleanup and making an image.

--- src/Entity/Brick/Image.php
+++ src/Entity/Brick/Image.php
@@ -11,11 +11,25 @@ use Drupal\eck\Entity\EckEntity;
  */
 class Image extends EckEntity {
 
-  public function label() {
-    kint($this->getImage());
-    return 'image';
-  }
 
+  public function label() {
+
+    $image = $this->getImage();
+
+    $title = $image->getValue()['title'];
+    $w = intval($image->getValue()['width']);
+    $h = intval($image->getValue()['height']);
+    $alt = $image->getValue()['alt'];
+
+    $label = $title ? $title : $alt;
+    $label = $label ? $label : 'no alt or title';
+
+    $f = $image->entity->url();
+
+    $f = '<br /><img src="'.$f.'" width="25" height="25" />';
+
+    return $label . ' ('.$w.'x'.$h.')' . $f;
+  }
 
   /**
    * @return \Drupal\image\Plugin\Field\FieldType\ImageItem
