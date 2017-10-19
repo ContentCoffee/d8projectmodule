56bf6f447fa3c8a80cfd220e99c32e719bd7e261
Robert Brown <robert.brown@contentcoffee.com>
2017-10-19 14:30:11 +0200

    Added a sectionTitle.

---
+++ src/Entity/Brick/SectionTitle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\project\Entity\Brick;
+
+use Drupal\eck\Entity\EckEntity;
+
+/**
+ * Class SectionTitle
+ * @package Drupal\project\Entity\Brick
+ */
+class SectionTitle extends EckEntity
+{
+
+}
